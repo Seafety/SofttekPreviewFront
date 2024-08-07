b42db8edab4abb92b1e89a46abfb476800f41cfc
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-06 22:25:36 -0300

    feat: :art: adding initial project structure
    adding initial project structure

---
+++ src/Pages/Home/RealTime/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RealTime() {
+  return <div>index</div>;
+}
---
+++ src/Pages/Times/Times.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Times() {
+  return <div>Times</div>;
+}
