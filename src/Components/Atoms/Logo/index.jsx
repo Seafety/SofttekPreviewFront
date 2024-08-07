a6d83899fa9a5c356d858b41ec3c2db6e50df28f
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-07 20:50:56 -0300

    feat: :sparkles: adding atomic component: logo and menu
    adding atomic component: logo and menu

---
+++ src/Components/Atoms/Logo/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { ReactComponent as SofttekPreviewLogo } from "../../../Assets/Logo.svg";
+const Logo = () => {
+  return <SofttekPreviewLogo />;
+};
+
+export default Logo;
---
+++ src/Components/Atoms/Menu/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { ReactComponent as BugerMenu } from "../../../Assets/menu.svg";
+const Menu = () => {
+  return <BugerMenu />;
+};
+
+export default Menu;
