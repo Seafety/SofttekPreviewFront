ad4db3db2ccf31bc1e3a6725c3253c8cee22303b
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-09 01:10:57 -0300

    feat: :construction: subnav construction in progress
    subnav construction in progress

--- src/Components/Atoms/MenuIcon/index.jsx
+++ src/Components/Atoms/MenuIcon/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import BugerMenu from "../../../Assets/menu.svg";
-const Menu = () => {
+const MenuIcon = () => {
   return <img src={BugerMenu} alt="" />;
 };
 
-export default Menu;
+export default MenuIcon;
---
+++ src/Components/Organisms/SubNavDash/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ModuleSelect from "../../Molecules/ModuleSelect";
+import styles from "./index.module.scss";
+const SubNavDash = () => {
+  return (
+    <section className={styles.subnave_section}>
+      <nav className={styles.subnave_section_links}>
+        <li>
+          <Link to="/realtime">Real-Time</Link>
+        </li>
+        <li>
+          <Link to="/forecasts">Forecast</Link>
+        </li>
+      </nav>
+      <ModuleSelect />
+    </section>
+  );
+};
+
+export default SubNavDash;
