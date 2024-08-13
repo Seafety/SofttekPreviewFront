fa62896faf5c131afbc7002083ff512c91875c1f
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-13 00:40:27 -0300

    feat: :sparkles: Adding Contract Information Table
    Adding Contract Information Table

--- src/Components/Organisms/ContractsSection/index.jsx
+++ src/Components/Organisms/ContractsSection/index.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import DashSection from "../../Templates/DashSectionTemplate";
 import styles from "./index.module.scss";
 import InfoBoxView from "../../Atoms/InfoBoxView";
-
+import alert from "../../../Assets/alert.svg";
 const ContractsSection = () => {
   const contractData = [
-    { type: "Desvio de Escopo", value: "Em Análise" },
-    { type: "Desvio de SLA", value: "Alto" },
-    { type: "Desvio Orçamentário", value: "Médio" },
-    { type: "Baixo NPS", value: "Baixo" },
+    { type: "Desvio de Escopo", value: "23" },
+    { type: "Desvio de SLA", value: "46" },
+    { type: "Desvio Orçamentário", value: "12" },
+    { type: "Baixo NPS", value: "3" },
   ];
 
   const infoBoxe = (
     <InfoBoxView
-      title="Total de Contratos"
+      title="Contratos Ativos"
       color="#0d296e"
       info={`325`}
       type="money"
@@ -33,7 +33,11 @@ const ContractsSection = () => {
           {contractData.map((item, index) => (
             <tr key={index}>
               <td>{item.type}</td>
-              <td>{item.value}</td>
+              <td>
+                {" "}
+                <img src={alert} alt="" />
+                {item.value}
+              </td>
             </tr>
           ))}
         </tbody>
