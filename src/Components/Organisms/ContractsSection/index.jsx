5f66dec08124bbc23bf40227d0a630dec4944cd4
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-12 23:54:01 -0300

    feat: :sparkles: adding initial structure of the contracts section
    adding initial structure of the contracts section

--- src/Components/Organisms/ContractsSection/index.jsx
+++ src/Components/Organisms/ContractsSection/index.jsx
@@ -1,9 +1,45 @@
 import React from "react";
-// import styles from "./index.module.scss";
 import DashSection from "../../Templates/DashSectionTemplate";
+import styles from "./index.module.scss";
+import InfoBoxView from "../../Atoms/InfoBoxView";
 
 const ContractsSection = () => {
-  return <DashSection title="Análise Financeira"></DashSection>;
+  const contractData = [
+    { type: "Desvio de Escopo", value: "Em Análise" },
+    { type: "Desvio de SLA", value: "Alto" },
+    { type: "Desvio Orçamentário", value: "Médio" },
+    { type: "Baixo NPS", value: "Baixo" },
+  ];
+
+  const infoBoxe = (
+    <InfoBoxView
+      title="Total de Contratos"
+      color="#0d296e"
+      info={`325`}
+      type="money"
+    />
+  );
+
+  return (
+    <DashSection title="Análise de Contratos" title_infobox={infoBoxe}>
+      <table className={styles.contractTable}>
+        <thead>
+          <tr>
+            <th>Tipo de Desvio</th>
+            <th>Quantidade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contractData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.type}</td>
+              <td>{item.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </DashSection>
+  );
 };
 
 export default ContractsSection;
