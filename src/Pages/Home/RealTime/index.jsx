116799b1bf79b37c42caa929ea0a4bf5d448ae0a
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-18 20:33:22 -0300

    feat: :sparkles: adding drag and drop to the dashboard
    adding drag and drop to the dashboard

--- src/Pages/Home/RealTime/index.jsx
+++ src/Pages/Home/RealTime/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.scss";
 import FinanceSection from "../../../Components/Organisms/FinanceSection";
 import ContractsSection from "../../../Components/Organisms/ContractsSection";
@@ -10,18 +10,114 @@ import ConclusionChart from "../../../Components/Organisms/ConclusionChart";
 import ChamadosConsultor from "../../../Components/Organisms/ChamadosConsultorSection";
 import ConsultoresDisponiveis from "../../../Components/Organisms/ConsultoresDisponiveisSection";
 
+const COMPONENTS = {
+  StatusArea,
+  FinanceSection,
+  ContractsSection,
+  ConsultoresDisponiveis,
+  OpenTickets,
+  ChamadosConsultor,
+  ConclusionChart,
+  CriticidadeBar,
+  ComplexChart,
+};
+
 const RealTime = () => {
+  const [leftColumn, setLeftColumn] = useState([
+    "StatusArea",
+    "FinanceSection",
+    "ContractsSection",
+  ]);
+
+  const [rightColumn, setRightColumn] = useState([
+    "OpenTickets",
+    "ConsultoresDisponiveis",
+    "ChamadosConsultor",
+    "ConclusionChart",
+    "CriticidadeBar",
+    "ComplexChart",
+  ]);
+
+  const handleDragStart = (e, index, column) => {
+    e.dataTransfer.setData("draggedIndex", index);
+    e.dataTransfer.setData("sourceColumn", column);
+  };
+
+  const handleDrop = (e, dropIndex, targetColumn, setColumn) => {
+    e.preventDefault();
+    const draggedIndex = parseInt(e.dataTransfer.getData("draggedIndex"));
+    const sourceColumn = e.dataTransfer.getData("sourceColumn");
+
+    let sourceColumnData, sourceSetColumn;
+
+    if (sourceColumn === "left") {
+      sourceColumnData = leftColumn;
+      sourceSetColumn = setLeftColumn;
+    } else {
+      sourceColumnData = rightColumn;
+      sourceSetColumn = setRightColumn;
+    }
+
+    const targetColumnData = targetColumn === "left" ? leftColumn : rightColumn;
+
+    const [draggedItem] = sourceColumnData.splice(draggedIndex, 1);
+    targetColumnData.splice(dropIndex, 0, draggedItem);
+
+    sourceSetColumn([...sourceColumnData]);
+    setColumn([...targetColumnData]);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className={styles.container}>
-      <StatusArea />
-      <FinanceSection />
-      <ContractsSection />
-      <ConsultoresDisponiveis />
-      <OpenTickets />
-      <ChamadosConsultor />
-      <ConclusionChart />
-      <CriticidadeBar />
-      <ComplexChart />
+      <div
+        className={styles.column}
+        onDragOver={handleDragOver}
+        onDrop={(e) => handleDrop(e, leftColumn.length, "left", setLeftColumn)}
+      >
+        {leftColumn.map((componentKey, index) => {
+          const Component = COMPONENTS[componentKey];
+          return (
+            <div
+              key={componentKey}
+              draggable
+              onDragStart={(e) => handleDragStart(e, index, "left")}
+              onDragOver={handleDragOver}
+              onDrop={(e) => handleDrop(e, index, "left", setLeftColumn)}
+              className={styles.draggableComponent}
+            >
+              <Component />
+            </div>
+          );
+        })}
+      </div>
+
+      <div
+        className={styles.column}
+        onDragOver={handleDragOver}
+        onDrop={(e) =>
+          handleDrop(e, rightColumn.length, "right", setRightColumn)
+        }
+      >
+        {rightColumn.map((componentKey, index) => {
+          const Component = COMPONENTS[componentKey];
+          return (
+            <div
+              key={componentKey}
+              draggable
+              onDragStart={(e) => handleDragStart(e, index, "right")}
+              onDragOver={handleDragOver}
+              onDrop={(e) => handleDrop(e, index, "right", setRightColumn)}
+              className={styles.draggableComponent}
+            >
+              <Component />
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
